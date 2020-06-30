9430af5b8a6f0dd0dafece37e2f472c77ea6fbfe
Matthew Castiglione <matt3castiglione@sbcglobal.net>
2020-06-29 19:17:16 -0500

    Finished Exercise 1

--- src/concurrency/Main.java
+++ src/concurrency/Main.java
@@ -4,5 +4,10 @@ public class Main {
    
     public static void main(String[] args) {
          //run all of your threads from this main class.
+        Thread reasoning = new Reasoning();
+        reasoning.run();
+
+        Thread team = new Thread(new TeamTC1());
+        team.run();
     }
 }
--- src/concurrency/Reasoning.java
+++ src/concurrency/Reasoning.java
@@ -1,10 +1,17 @@
 package concurrency;
 
-class Reasoning {
+class Reasoning extends Thread{
     //set up this class so it can become a valid thread. 
     void distinguish() {
         //print to the console the difference between a thread and a process
         //print out you think will happen if you invoke the run() method of a thread as opposed to the start() method of a thread.
+        System.out.println("A process is any program in execution. A thread is a segment of a process");
+        System.out.println("Using the run method will use the current thread and run it while start creates a new thread and runs it");
+    }
+
+    @Override
+    public void run() {
+        distinguish();
     }
 }
 
